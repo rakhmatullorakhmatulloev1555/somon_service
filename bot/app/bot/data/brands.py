b66016eacd4b8fa76625b798d05ba913b0dc1237
Rakhmatullo <65566629+rakhmatullorakhmatulloev1555@users.noreply.github.com>
2026-02-13 09:02:38 +0500

    Add files via upload

---
+++ bot/app/bot/data/brands.py
@@ -0,0 +1,318 @@
+BRANDS = {
+# 💻 Ноутбуки и ПК
+"💻 Ноутбуки": [
+    "HP",
+    "Dell",
+    "Lenovo",
+    "Asus",
+    "Acer",
+    "Apple",
+    "MSI",
+    "Huawei",
+    "Xiaomi",
+    "Digma",
+    "Другое"
+    ],
+"🖥 ПК": [
+    "Intel",
+    "AMD",
+    "Custom",
+    "HP",
+    "Dell",
+    "Lenovo",
+    "DNS",
+    "Acer",
+    "Asus",
+    "Другое"
+],
+
+"🗄 Серверы": [
+    "HP",
+    "Dell",
+    "Lenovo",
+    "Supermicro",
+    "Cisco",
+    "IBM",
+    "Fujitsu",
+    "Другое"
+],
+
+"📊 1С Бухгалтерия": [
+    "1С",
+    "Другое"
+],
+
+"🖨 МФУ и Принтеры": [
+    "HP",
+    "Canon",
+    "Epson",
+    "Xerox",
+    "Brother",
+    "Pantum",
+    "Kyocera",
+    "Ricoh",
+    "Другое"
+],
+
+# 🏠 Бытовая Техника
+"📺 ТВ и Смарт ТВ": [
+    "Samsung",
+    "LG",
+    "Sony",
+    "Xiaomi",
+    "TCL",
+    "Hisense",
+    "Philips",
+    "Artel",
+    "Sharp",
+    "Panasonic",
+    "Другое"
+],
+
+"🧺 Стиральные машины": [
+    "LG",
+    "Samsung",
+    "Bosch",
+    "Indesit",
+    "Artel",
+    "Midea",
+    "Hansa",
+    "Electrolux",
+    "Whirlpool",
+    "Candy",
+    "Другое"
+],
+
+"❄️ Кондиционеры и сплит-системы": [
+    "LG",
+    "Samsung",
+    "Midea",
+    "Gree",
+    "Hisense",
+    "Artel",
+    "Chigo",
+    "Tadiran",
+    "Haier",
+    "Panasonic",
+    "Ballu",
+    "Другое"
+],
+
+"🍽 Посудомоечные машины": [
+    "Bosch",
+    "Siemens",
+    "Electrolux",
+    "Hansa",
+    "Midea",
+    "AEG",
+    "Hotpoint-Ariston",
+    "Candy",
+    "Другое"
+],
+
+"🔥 Водонагреватели (бойлеры)": [
+    "Ariston",
+    "Thermex",
+    "Timberk",
+    "Electrolux",
+    "Midea",
+    "Atlantic",
+    "Baxi",
+    "Другое"
+],
+
+"🔊 Аудиосистемы": [
+    "JBL",
+    "Sony",
+    "LG",
+    "Xiaomi",
+    "Philips",
+    "Dyson",
+    "Samsung",
+    "Harman/Kardon",
+    "Bang & Olufsen",
+    "Другое"
+],
+
+"🌬 Вентиляторы": [
+    "Vitek",
+    "Scarlett",
+    "Polaris",
+    "Midea",
+    "Zanussi",
+    "Bork",
+    "Rowenta",
+    "Другое"
+],
+
+"📡 Микроволновые печи": [
+    "Samsung",
+    "LG",
+    "Panasonic",
+    "Midea",
+    "Gorenje",
+    "Artel",
+    "Sharp",
+    "BBK",
+    "Другое"
+],
+
+"🤖 Роботы-пылесосы": [
+    "Xiaomi",
+    "Roborock",
+    "iRobot",
+    "Samsung",
+    "Dreame",
+    "Ecovacs",
+    "Hobot",
+    "Другое"
+],
+
+"🍳 Кухонные плиты": [
+    "Hansa",
+    "Gorenje",
+    "Bosch",
+    "Artel",
+    "Midea",
+    "Korting",
+    "Samsung",
+    "LG",
+    "Electrolux",
+    "Другое"
+],
+
+"♨️ Отопительные приборы": [
+    "Timberk",
+    "Electrolux",
+    "Ballu",
+    "Polaris",
+    "Scarlett",
+    "Vitek",
+    "Другое"
+],
+
+"🧹 Пылесосы": [
+    "Samsung",
+    "LG",
+    "Philips",
+    "Thomas",
+    "Karcher",
+    "Dyson",
+    "Rowenta",
+    "Miele",
+    "Другое"
+],
+
+"🍖 Электрические плитки и грили": [
+    "Tefal",
+    "Scarlett",
+    "Vitek",
+    "Moulinex",
+    "Maxwell",
+    "Polaris",
+    "Другое"
+],
+
+"🥩 Мясорубки": [
+    "Moulinex",
+    "Bosch",
+    "Philips",
+    "Panasonic",
+    "Scarlett",
+    "Kenwood",
+    "Zelmer",
+    "Другое"
+],
+
+"👕 Утюги": [
+    "Philips",
+    "Tefal",
+    "Rowenta",
+    "Bosch",
+    "Scarlett",
+    "Braun",
+    "Samsung",
+    "Другое"
+],
+
+# 🔐 Системы автоматизации и безопасности
+"🌐 СКС (Локальная сеть)": [
+    "TP-Link",
+    "D-Link",
+    "Cisco",
+    "Huawei",
+    "Ubiquiti",
+    "Zyxel",
+    "MikroTik",
+    "Juniper",
+    "HP Aruba",
+    "Другое"
+],
+
+"🚨 ОПС сигнализация": [
+    "Bolid",
+    "Rubezh",
+    "Ajax",
+    "Pyronix",
+    "Jablotron",
+    "DSC",
+    "Другое"
+],
+
+"📹 CCTV видеонаблюдение": [
+    "Hikvision",
+    "Dahua",
+    "Ezviz",
+    "HiWatch",
+    "TP-Link",
+    "Axis",
+    "Vstarcam",
+    "Другое"
+],
+
+"🏡 Умный Дом": [
+    "Xiaomi",
+    "Aqara",
+    "Tuya",
+    "Google Nest",
+    "Apple HomeKit",
+    "Philips Hue",
+    "Yandex",
+    "Другое"
+],
+
+"🏢 BMS автоматизация зданий": [
+    "Siemens",
+    "Schneider Electric",
+    "ABB",
+    "Honeywell",
+    "Johnson Controls",
+    "Другое"
+],
+
+"📊 SCADA": [
+    "WinCC",
+    "Trace Mode",
+    "AVEVA",
+    "InduSoft",
+    "Другое"
+],
+
+# 🤖 Интеграция Telegram
+"📲 Интеграция Telegram + 1С": [
+    "Битрикс24",
+    "Custom Solution",
+    "Comagic",
+    "Roistat",
+    "Другое"
+],
+
+"🚪 Telegram + Турникеты": [
+    "Perco",
+    "ZKTeco",
+    "Custom Solution",
+    "IronLogic",
+    "Parsec",
+    "Другое"
+]
+}
