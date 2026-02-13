345060b29c4dd694501fb72effa5dd1ec9576dfd
Rakhmatullo <65566629+rakhmatullorakhmatulloev1555@users.noreply.github.com>
2026-02-13 09:15:40 +0500

    Create __init__.py

---
+++ bot/app/bot/states/__init__.py
@@ -0,0 +1,11 @@
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class AddMaster(StatesGroup):
+
+    name = State()
+    surname = State()
+    phone = State()
+    specialization = State()
+    experience = State()
+    skills = State()
